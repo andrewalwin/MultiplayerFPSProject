953a34463f6779cc39d0506b1ac1825ae3a4aff8
Andrew Alwin <aalwin@callutheran.edu>
2018-03-06 20:26:37 -0800

    BasicMovementAndModel
    Added a player model (courtesy of Brackeys) and added basic movement animation using a BlendTreee to blend between 3 states of animation.

--- MultiplayerFPS/Assets/Scripts/PlayerController.cs
+++ MultiplayerFPS/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(ConfigurableJoint))]
 [RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour {
@@ -21,9 +22,11 @@ public class PlayerController : MonoBehaviour {
     private float jointSpring = 20f;
     [SerializeField]
     private float jointMaxForce = 40f;
-
+    
+    //component caching
     private PlayerMotor motor;
     private ConfigurableJoint joint;
+    private Animator animator;
 
     void Start()
     {
@@ -32,21 +35,25 @@ public class PlayerController : MonoBehaviour {
         //require a joint and set it here instead of in inspector
         joint = GetComponent<ConfigurableJoint>();
 
+        animator = GetComponent<Animator>();
         SetJointSettings(jointSpring);
     }
 
     void Update()
     {
         //calculate movement as a 3d vector, doing Raw to get complete control of movement, without raw unity performs its own smoothing
-        float xMov = Input.GetAxisRaw("Horizontal");
-        float zMov = Input.GetAxisRaw("Vertical");
+        float xMov = Input.GetAxis("Horizontal");
+        float zMov = Input.GetAxis("Vertical");
 
         //transform.right instead of vector3.right is local, takes into account our current rotation
         Vector3 moveHorizontal = transform.right * xMov;
         Vector3 moveVertical = transform.forward * zMov;
 
         //normalize so theres no varying speed, always normal vector(just for direction, speed is for the actual speed)
-        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+        Vector3 velocity = (moveHorizontal + moveVertical) * speed;
+
+        //Animate movement
+        animator.SetFloat("ForwardVelocity", zMov);
 
         //Apply movement
         motor.Move(velocity);
