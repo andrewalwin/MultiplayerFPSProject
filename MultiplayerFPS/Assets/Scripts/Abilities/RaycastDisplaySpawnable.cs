0501371a06271c90ef7e6329fd8528fa99940361
Andrew Alwin <aalwin@callutheran.edu>
2018-11-11 21:08:01 -0800

    RaycastSpawnable pt.1
    Added part of a script to display a possible spawnable object for the player to decid where to spawn it. Right now it just displays the object at the hit objects location with the objects up as the hit objects normal. Need to add spawning capabilities as well as showing whether or not the object is "valid" to spawn, and rotating/mouse wheel to move it closer/farther (maybe). Also can't spawn object if the angle between the player up and the hit objects normal is bigger than a chosen float.

---
+++ MultiplayerFPS/Assets/Scripts/Abilities/RaycastDisplaySpawnable.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastDisplaySpawnable : MonoBehaviour {
+
+    //object has to have the same forward as the object you want to spawn, or else it'll spawn at weird angles
+    [SerializeField]
+    private GameObject spawnableObject;
+    private GameObject spawnableIns;
+
+    [SerializeField]
+    private Camera cam;
+
+    [SerializeField]
+    private float spawnRange;
+    [SerializeField]
+    private float spawnAngleMax;
+
+    [SerializeField]
+    private LayerMask ignoreCollisions;
+
+    private Vector3 spawnableRotation;
+
+    private bool isDisplaying = false;
+	private bool canSpawn;
+
+    [SerializeField]
+    private KeyCode abilityKey;
+    
+    void Start()
+    {
+        if (cam == null)
+        {
+            cam = GetComponent<Camera>();
+        }
+        //can do our creation of spawnableobject here if we don't use a prefab
+        if (spawnableObject != null)
+        {
+            spawnableIns = GameObject.Instantiate(spawnableObject);
+            spawnableIns.SetActive(false);
+        }
+
+        spawnableRotation = transform.forward;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log("Current displayable: " + isDisplaying);
+            isDisplaying = !isDisplaying;
+        }
+
+        if (isDisplaying)
+        {
+            if (Input.GetMouseButton(1))
+            {
+                //spawnableRotation = (spawnableRotation + 2.0f) % 360;
+            }
+            TryDisplaySpawnable();
+        }
+        else
+        {
+            spawnableIns.SetActive(false);
+            canSpawn = false;
+        }
+    }
+
+    void TryDisplaySpawnable()
+    {
+        if (cam != null)
+        {
+            Vector3 rayOrigin = cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+
+            if (Physics.Raycast(rayOrigin, cam.transform.forward, out hit, spawnRange))
+            {
+                spawnableIns.transform.position = hit.point;
+                spawnableIns.transform.forward = spawnableRotation;
+                spawnableIns.transform.up = hit.normal;
+                if (Vector3.Angle(hit.normal, Vector3.up) <= spawnAngleMax)
+                {
+                    //make color "valid" maybe so player knows they can spawn it
+                    //maybe enable above so we're not CONSTANTLY enabling the object
+                    spawnableIns.SetActive(true);
+                    canSpawn = true;
+                }
+                else
+                {
+                    Debug.Log("Invalid");
+                    //make color "invalid" maybe
+                    spawnableIns.SetActive(false);
+                    canSpawn = false;
+                }
+            }
+            else
+            {
+                spawnableIns.SetActive(false);
+                canSpawn = false;
+            }
+        }
+    }
+}
